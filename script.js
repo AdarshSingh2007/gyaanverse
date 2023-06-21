e476c4571a2bd8ac0d7a9cd2fca19f3f7db86d9b
Adarsh Singh <69581997+AdarshSingh2007@users.noreply.github.com>
2023-06-21 22:45:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 6000); // Change slide every 6 seconds
+}
+
+function changeSlide(n) {
+  slideIndex += n;
+  var slides = document.getElementsByClassName("mySlides");
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
